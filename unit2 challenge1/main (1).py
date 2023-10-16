2145924d42b4225a51911a3db4dca6af31b3063e
9D0937E5160E79F84A1451165086EEFB <148037299+9D0937E5160E79F84A1451165086EEFB@users.noreply.github.com>
2023-10-16 11:34:56 +0530

    Add files via upload

---
+++ unit2 challenge1/main (1).py
@@ -0,0 +1,26 @@
+class Bank:
+  def__init__(self):
+     self.balance =0
+     print("the account is created")
+  def deposit (self):
+     amount=float(input ("Enter the amount to deposit:")
+         self.balance=self.balance+amount
+    print ("deposit is successful and the balance in the account is %f"%self.balance)
+def withdraw (self):
+  amount=float (input ("enter the amount to withdraw"))
+if(self.balance>=amount):
+  self.balance=self.balance-amount and balance
+  print("the withdraw is successfull is %f"%self.balance)
+else:
+  print('insufficient balance')
+def enquiry(self):
+  print ("the balance in the account is %f"%self.balance)
+acc=Bank()
+acc.deposit()
+acc.withdraw()
+acc.enquiry()
+
+
+  
+
+                  
